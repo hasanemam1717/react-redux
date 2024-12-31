54eaff504f362b25c13695bad82c6ed0de71166e
hasanemam1717 <hasanemam1717@gmail.com>
2025-01-01 05:02:49 +0600

    create redux store and wrap with provider in store

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>To do app with React and redux</h1>
+        <div>0</div>
+        <button>increment</button>
+        <button>decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
